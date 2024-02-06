60a84ae1860e282c94ad6a9dd1c2fe21c4b6ff6f
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-02-06 18:45:54 -0500

    Feat: Page container now can display current page title

--- src/components/layout/PageContainer.tsx
+++ src/components/layout/PageContainer.tsx
@@ -1,8 +1,15 @@
 import "../../styles/layout/PageContainer.css";
 import { ReactNode } from "react";
 
-const PageContainer = (props: { children: ReactNode }) => {
-  return <div className="page-container">{props.children}</div>;
+const PageContainer = (props: { title?: string; children: ReactNode }) => {
+  return (
+    <div className="page-container">
+      <div className="main-page-header">
+        <h1>{props.title}</h1>
+      </div>
+      {props.children}
+    </div>
+  );
 };
 
 export default PageContainer;
