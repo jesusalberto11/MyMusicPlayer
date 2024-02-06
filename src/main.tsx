6e47485323bf9b0dea63dce39bae52fd6e9d24f5
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-02-06 18:59:22 -0500

    Feat: Implemented router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import "./styles.css";
+import { router } from "./router/index";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
