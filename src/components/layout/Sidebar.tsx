e6181601c112552f6477738aebc5179ebe11cc18
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-02-06 18:48:40 -0500

    Feat: Implemented sidebar item on the sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
-import { SVG_ICONS } from "../../helpers/svgIcons";
 import "../../styles/layout/Sidebar.css";
-import SVGIcon from "../shared/SVGIcon";
+import { SVG_ICONS } from "../../helpers/svgIcons";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
   return (
@@ -8,27 +8,20 @@ const Sidebar = () => {
       <input type="search" className="searchbar" placeholder="Search" />
       <nav className="sidebar-nav">
         <hr />
-        <div className="sidebar-nav-item">
-          <SVGIcon icon={SVG_ICONS.MUSIC_NOTE} />
-          Songs
-        </div>
-        <div className="sidebar-nav-item">
-          <SVGIcon icon={SVG_ICONS.MUSIC_ALBUM} />
-          Albums
-        </div>
-        <div className="sidebar-nav-item">
-          <SVGIcon icon={SVG_ICONS.FILE_MUSIC} />
-          Artists
-        </div>
-        <div className="sidebar-nav-item">
-          <SVGIcon icon={SVG_ICONS.PLAY_LIST} />
-          Playlists
-        </div>
+        <SidebarItem to="/" title="Songs" icon={SVG_ICONS.MUSIC_NOTE} />
+        <SidebarItem to="/albums" title="Albums" icon={SVG_ICONS.MUSIC_ALBUM} />
+        <SidebarItem
+          to="/artists"
+          title="Artists"
+          icon={SVG_ICONS.FILE_MUSIC}
+        />
+        <SidebarItem
+          to="/playlists"
+          title="Playlists"
+          icon={SVG_ICONS.PLAY_LIST}
+        />
         <hr />
-        <div className="sidebar-nav-item">
-          <SVGIcon icon={SVG_ICONS.SETTINGS} />
-          Settings
-        </div>
+        <SidebarItem to="settings" title="Settings" icon={SVG_ICONS.SETTINGS} />
       </nav>
     </div>
   );
