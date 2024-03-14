704021b40e84b74dcbb8d2762bf47b6165ba4d51
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-14 16:51:39 -0500

    Feat: Added play/pause logic

--- src/components/music player/MusicPlayerControls.tsx
+++ src/components/music player/MusicPlayerControls.tsx
@@ -1,8 +1,11 @@
 import "../../styles/music player/MusicPlayerControls.css";
 import { SVG_ICONS } from "../../helpers/svgIcons";
 import SimpleButton from "../shared/SimpleButton";
+import { useMusicPlayer } from "../../hooks/useMusicPlayer";
 
 const MusicPlayerControls = () => {
+  const { isSongPlaying, setPlay, setPause } = useMusicPlayer();
+
   return (
     <div className="music-player-controls">
       <div className="music-player-buttons">
@@ -11,11 +14,19 @@ const MusicPlayerControls = () => {
           title="Previous song"
           icon={SVG_ICONS.SKIP_START}
         />
-        <button className="play-button">
-          <svg width="32" height="32" fill="#fff" viewBox="0 0 16 16">
-            <path d={SVG_ICONS.PLAY} />
-          </svg>
-        </button>
+        {isSongPlaying ? (
+          <button className="play-button" onClick={() => setPause()}>
+            <svg width="32" height="32" fill="#fff" viewBox="0 0 16 16">
+              <path d={SVG_ICONS.PAUSE} />
+            </svg>
+          </button>
+        ) : (
+          <button className="play-button" onClick={() => setPlay()}>
+            <svg width="32" height="32" fill="#fff" viewBox="0 0 16 16">
+              <path d={SVG_ICONS.PLAY} />
+            </svg>
+          </button>
+        )}
         <SimpleButton
           showTitle={false}
           title="Next song"
