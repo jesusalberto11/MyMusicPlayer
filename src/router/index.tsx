124c5931e4d1ddef3dd868728adad06baf9c29cc
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-14 16:50:35 -0500

    Update: Now Music list its main route of the app

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../pages/ErrorPage";
 import SettingsPage from "../pages/SettingsPage";
+import MainPage from "../pages/MainPage";
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ export const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
       {
         path: "settings",
         element: <SettingsPage />,
