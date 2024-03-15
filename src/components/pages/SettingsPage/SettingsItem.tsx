83896fcc8e93a9e3676848460634c36df27bdfe5
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-14 20:49:32 -0500

    Update: Updated component to show icon and description

--- src/components/pages/SettingsPage/SettingsItem.tsx
+++ src/components/pages/SettingsPage/SettingsItem.tsx
@@ -1,10 +1,22 @@
 import { ReactNode } from "react";
 import "../../../styles/pages/SettingsPage/SettingsItem.css";
+import SVGIcon from "../../shared/SVGIcon";
 
-const SettingsItem = (props: { title: string; children: ReactNode }) => {
+const SettingsItem = (props: {
+  title: string;
+  description: string;
+  icon: string;
+  children: ReactNode;
+}) => {
   return (
     <div className="settings-item-container">
-      <p className="settings-item-title">{props.title}</p>
+      <div className="flex centered" style={{ gap: "15px" }}>
+        <SVGIcon icon={props.icon} />
+        <div className="flex flex-column" style={{ gap: "5px" }}>
+          <p className="settings-item-title">{props.title}</p>
+          <p className="settings-item-description">{props.description}</p>
+        </div>
+      </div>
       {props.children}
     </div>
   );
