3b6ea921981adf2761d21fb40e2df9a5f9309299
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-15 12:02:27 -0500

    Feat: Added text translations

--- src/components/pages/SettingsPage/ChangeTheme.tsx
+++ src/components/pages/SettingsPage/ChangeTheme.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useTheme } from "../../../hooks/useTheme";
+import { useTranslation } from "react-i18next";
 
 const ChangeTheme = () => {
+  const [t] = useTranslation("global");
   const { selectedTheme, checkSavedTheme, changeTheme } = useTheme();
 
   useEffect(() => {
@@ -11,7 +13,7 @@ const ChangeTheme = () => {
   return (
     <div className="theme-select">
       <label htmlFor="select-theme" hidden>
-        Select app theme
+        {t("SETTINGS-PAGE.CHANGE-THEME-DESC")}
       </label>
       <select
         id="select-theme"
@@ -20,8 +22,8 @@ const ChangeTheme = () => {
         value={selectedTheme}
         onChange={(e) => changeTheme(e.target.value)}
       >
-        <option value="dark">Dark (Recomended)</option>
-        <option value="light">Light</option>
+        <option value="dark">{t("SETTINGS-PAGE.CHANGE-THEME-DARK")}</option>
+        <option value="light">{t("SETTINGS-PAGE.CHANGE-THEME-LIGHT")}</option>
       </select>
     </div>
   );
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,22 +1,25 @@
+import { useTranslation } from "react-i18next";
 import PageContainer from "../components/layout/PageContainer";
 import ChangeTheme from "../components/pages/SettingsPage/ChangeTheme";
 import SettingsItem from "../components/pages/SettingsPage/SettingsItem";
 import { SVG_ICONS } from "../helpers/svgIcons";
 
 const SettingsPage = () => {
+  const [t] = useTranslation("global");
+
   return (
-    <PageContainer title="Settings">
+    <PageContainer title={t("SETTINGS-PAGE.SETTINGS-PAGE-TITLE")}>
       <hr />
       <SettingsItem
-        title="Language"
-        description="Cambia el idioma de la aplicación."
+        title={t("SETTINGS-PAGE.CHANGE-LANG-TITLE")}
+        description={t("SETTINGS-PAGE.CHANGE-LANG-DESC")}
         icon={SVG_ICONS.THEME}
       >
         <p>Change lang</p>
       </SettingsItem>
       <SettingsItem
-        title="Theme"
-        description="Cambia el lenguaje de la aplicación."
+        title={t("SETTINGS-PAGE.CHANGE-THEME-TITLE")}
+        description={t("SETTINGS-PAGE.CHANGE-THEME-DESC")}
         icon={SVG_ICONS.TRANSLATE}
       >
         <ChangeTheme />
