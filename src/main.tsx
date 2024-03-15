fb46ee0ab8f6c2a0d268fbc01b85dc6bab86f387
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-15 12:01:51 -0500

    Feat: Implemented Reacti18next config context

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./styles.css";
 import { router } from "./router/index";
+
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+import global_es from "./i18n/es/global.json";
+import global_en from "./i18n/en/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <I18nextProvider i18n={i18next}>
+      <RouterProvider router={router} />
+    </I18nextProvider>
   </React.StrictMode>
 );
