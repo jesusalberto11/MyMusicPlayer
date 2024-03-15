f09c197a8a46a25268b7c400d1eff9999eec6300
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-14 20:30:13 -0500

    Feat: Created hook useTheme

---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export const useTheme = () => {
+  const [selectedTheme, setSelectedTheme] = useState<string>("");
+
+  const checkSavedTheme = () => {
+    console.log("Saved theme: " + localStorage.getItem("theme"));
+    if (localStorage.getItem("theme")) {
+      const theme = localStorage.getItem("theme") || "dark";
+      changeTheme(theme);
+    }
+  };
+
+  const changeTheme = (value: string) => {
+    if (value === "light") {
+      setSelectedTheme("light");
+
+      document.body.classList.add("light-mode");
+      localStorage.setItem("theme", "light");
+    }
+
+    if (value === "dark") {
+      setSelectedTheme("dark");
+
+      document.body.classList.remove("light-mode");
+      localStorage.setItem("theme", "dark");
+    }
+  };
+
+  return {
+    selectedTheme,
+    checkSavedTheme,
+    changeTheme,
+  };
+};
