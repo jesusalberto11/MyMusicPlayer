0f61de4cec1ff6db4d5cd6ccab550a88214ffe9c
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-14 20:50:04 -0500

    Update: Updated settings items components to have icon and description

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,15 +1,24 @@
 import PageContainer from "../components/layout/PageContainer";
 import ChangeTheme from "../components/pages/SettingsPage/ChangeTheme";
 import SettingsItem from "../components/pages/SettingsPage/SettingsItem";
+import { SVG_ICONS } from "../helpers/svgIcons";
 
 const SettingsPage = () => {
   return (
     <PageContainer title="Settings">
       <hr />
-      <SettingsItem title="Change Language">
+      <SettingsItem
+        title="Language"
+        description="Cambia el idioma de la aplicación."
+        icon={SVG_ICONS.THEME}
+      >
         <p>Change lang</p>
       </SettingsItem>
-      <SettingsItem title="Change Theme">
+      <SettingsItem
+        title="Theme"
+        description="Cambia el lenguaje de la aplicación."
+        icon={SVG_ICONS.TRANSLATE}
+      >
         <ChangeTheme />
       </SettingsItem>
     </PageContainer>
