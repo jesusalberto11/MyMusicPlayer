90fd3dc82de454585492c98db8a9e7f50e2f1d72
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-14 20:32:10 -0500

    Feat: Added ChangeTheme settings item

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,9 +1,17 @@
 import PageContainer from "../components/layout/PageContainer";
+import ChangeTheme from "../components/pages/SettingsPage/ChangeTheme";
+import SettingsItem from "../components/pages/SettingsPage/SettingsItem";
 
 const SettingsPage = () => {
   return (
     <PageContainer title="Settings">
-      <h1>Hello world from settings page</h1>
+      <hr />
+      <SettingsItem title="Change Language">
+        <p>Change lang</p>
+      </SettingsItem>
+      <SettingsItem title="Change Theme">
+        <ChangeTheme />
+      </SettingsItem>
     </PageContainer>
   );
 };
