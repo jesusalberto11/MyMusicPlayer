e13735d3c663c144ac119d09a417ed6c9da41111
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-16 16:52:28 -0500

    Feat: Implemented text translations

--- src/components/pages/MainPage/MainPageHeader.tsx
+++ src/components/pages/MainPage/MainPageHeader.tsx
@@ -1,29 +1,32 @@
+import { useTranslation } from "react-i18next";
 import { SVG_ICONS } from "../../../helpers/svgIcons";
 import "../../../styles/pages/MainPageHeader.css";
 import SVGIcon from "../../shared/SVGIcon";
 
 const MainPageHeader = (props: { filterSongList: any }) => {
+  const [t] = useTranslation("global");
+
   return (
     <div className="main-page-header-container">
       <input
         type="search"
         onChange={(e) => props.filterSongList(e.target.value.toLowerCase())}
         className="searchbar"
-        placeholder="Search a song..."
+        placeholder={t("MAIN-PAGE-HEADER.SEARCH-SONG")}
       />
       <div className="header-items-container">
         <div className="header-item">
-          <label htmlFor="sort">Sort by:</label>
+          <label htmlFor="sort">{t("MAIN-PAGE-HEADER.SORT-BY")}</label>
           <select id="sort">
             <option>A - Z</option>
-            <option>Artist</option>
-            <option>Album</option>
-            <option>Year</option>
+            <option>{t("MAIN-PAGE-HEADER.SORT-BY-ARTIST")}</option>
+            <option>{t("MAIN-PAGE-HEADER.SORT-BY-ALBUM")}</option>
+            <option>{t("MAIN-PAGE-HEADER.SORT-BY-YEAR")}</option>
           </select>
         </div>
         <button className="random-button" type="button">
           <SVGIcon icon={SVG_ICONS.SHUFFLE} />
-          Pick a random song
+          {t("MAIN-PAGE-HEADER.PICK-RANDOM")}
         </button>
       </div>
     </div>
