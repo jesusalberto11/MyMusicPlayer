8a38e759c2a2cb2f645cf902753da70e18d10a21
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-18 13:45:20 -0500

    Update: Volume control panel now its a html dialog elemnt

--- src/components/music player/VolumeControl.tsx
+++ src/components/music player/VolumeControl.tsx
@@ -1,10 +1,11 @@
 import "../../styles/music player/VolumeControl.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMusicPlayer } from "../../hooks/useMusicPlayer";
 import SimpleButton from "../shared/SimpleButton";
 import { SVG_ICONS } from "../../helpers/svgIcons";
 
 const VolumeControl = () => {
+  const volumePanelRef = useRef<HTMLDialogElement | null>(null);
   const { changePlayerVolume } = useMusicPlayer();
   const [volumen, setVolumen] = useState("100");
   const [isVolumePanelOpen, setIsVolumePanelOpen] = useState(false);
@@ -13,26 +14,56 @@ const VolumeControl = () => {
     changePlayerVolume(Number(volumen) / 100);
   }, [volumen]);
 
+  useEffect(() => {
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape" && isVolumePanelOpen) {
+        closeVolumePanel();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [isVolumePanelOpen]);
+
+  const openVolumePanel = () => {
+    volumePanelRef.current?.showModal();
+    setIsVolumePanelOpen(true);
+  };
+
+  const closeVolumePanel = () => {
+    volumePanelRef.current?.close();
+    setIsVolumePanelOpen(false);
+  };
+
   return (
     <div className="volume-control-container">
-      {isVolumePanelOpen && (
-        <div className="volume-panel">
-          <label htmlFor="vol" hidden>
-            Volume (between 0 and 100):
-          </label>
-          <input
-            type="range"
-            id="vol"
-            name="vol"
-            min="0"
-            max="100"
-            value={volumen}
-            onChange={(e) => setVolumen(e.target.value)}
-            onBlur={isVolumePanelOpen && (() => setIsVolumePanelOpen(false))}
-          />
-          <p>{volumen}</p>
-        </div>
-      )}
+      <div className="dialog-container">
+        {isVolumePanelOpen && (
+          <dialog
+            ref={volumePanelRef}
+            open={isVolumePanelOpen}
+            className="volume-panel"
+          >
+            <label htmlFor="vol" hidden>
+              Volume (between 0 and 100):
+            </label>
+            <input
+              type="range"
+              id="vol"
+              name="vol"
+              min="0"
+              max="100"
+              value={volumen}
+              onChange={(e) => setVolumen(e.target.value)}
+              onBlur={isVolumePanelOpen && (() => closeVolumePanel())}
+            />
+            <p>{volumen}</p>
+          </dialog>
+        )}
+      </div>
       <SimpleButton
         showTitle={false}
         title="Open volume control"
@@ -46,7 +77,7 @@ const VolumeControl = () => {
         onClickItem={
           isVolumePanelOpen
             ? () => setIsVolumePanelOpen(false)
-            : () => setIsVolumePanelOpen(true)
+            : () => openVolumePanel()
         }
       />
     </div>
