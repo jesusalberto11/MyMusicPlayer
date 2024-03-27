49ea69ea52cc8f7168eb1a7783d46be2029e2a24
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-03-26 19:39:29 -0500

    Update: Changed type of props to string OR null

--- src/components/songs/SongImage.tsx
+++ src/components/songs/SongImage.tsx
@@ -1,7 +1,7 @@
 import "../../styles/pages/MainPage.css";
 import noCoverImg from "../../assets/no-cover.png";
 
-const SongImage = (props: { imageSrc: string; height?: string }) => {
+const SongImage = (props: { imageSrc: string | null; height?: string }) => {
   const checkHeight = () => {
     return props.height ? `${props.height}px` : "50px";
   };
@@ -10,7 +10,7 @@ const SongImage = (props: { imageSrc: string; height?: string }) => {
       src={props?.imageSrc ? props?.imageSrc : noCoverImg}
       alt="song-cover"
       className="song-list-item-img"
-      style={{ height: checkHeight() }}
+      style={{ height: checkHeight(), width: "auto" }}
       loading="lazy"
     />
   );
