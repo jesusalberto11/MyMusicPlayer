72cd11f8ee2bab7bd874ba73999360c51b0b7257
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-04-04 16:58:40 -0500

    Feat: Created use playlist custom hook

---
+++ src/hooks/usePlaylists.tsx
@@ -0,0 +1,51 @@
+import localforage from "localforage";
+import { ISong } from "../interfaces/ISong";
+import { IPlaylist } from "../interfaces/IPlaylist";
+import { usePlaylistStore } from "../store/PlaylistsStore";
+
+export const usePlaylists = () => {
+  const { playlists, setPlaylists } = usePlaylistStore();
+
+  const checkSavedPlaylists = () => {
+    localforage
+      .getItem("playlists")
+      .then((data) => {
+        setPlaylists(data as IPlaylist[]);
+      })
+      .catch((error) => {
+        console.error("Error getting playlists from localForage! ", error);
+      });
+  };
+
+  const createNewPlaylist = (name: string, songs: Array<ISong>) => {
+    const newPlaylist: IPlaylist = {
+      id: crypto.randomUUID(),
+      name: name,
+      songs: songs,
+      cover_image: songs.length > 0 ? songs[0]?.image : null,
+    };
+
+    setPlaylists([...playlists, newPlaylist]);
+
+    localforage.setItem("playlists", [...playlists, newPlaylist]);
+  };
+
+  const getPlaylist = async (id: string): Promise<IPlaylist | null> => {
+    return new Promise((resolve, reject) => {
+      const foundPlaylist = playlists.find((playlist) => playlist.id === id);
+
+      if (foundPlaylist) {
+        resolve(foundPlaylist);
+      } else {
+        reject(new Error("Can't find playlist with that id!"));
+      }
+    });
+  };
+
+  return {
+    playlists,
+    checkSavedPlaylists,
+    createNewPlaylist,
+    getPlaylist,
+  };
+};
